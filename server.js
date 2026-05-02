63d1111bafad05c6f234657cee4cf0ad0d4d9ee0
Dileep Kumar <mdileepkumar578@gmail.com>
2026-05-02 18:18:37 +0530

    fix crash

--- server.js
+++ server.js
@@ -9,16 +9,8 @@ connectDB();
 
 const app = express();
 
-
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
-
-app.options("*", cors());
-
+// ✅ simple and safe
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -27,13 +19,12 @@ app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-// Test
 app.get("/", (req, res) => {
-    res.send("API Running...");
+  res.send("API Running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });

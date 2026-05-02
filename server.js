767947e56e0cda6d706c1fda74468dd1cac65e1e
Dileep Kumar <mdileepkumar578@gmail.com>
2026-05-02 18:07:59 +0530

    fix cors for production

--- server.js
+++ server.js
@@ -4,37 +4,38 @@ const cors = require("cors");
 
 dotenv.config();
 
-// connectDB
 const connectDB = require("./config/db");
 connectDB();
 
 const app = express();
 
-// Middleware
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    "https://team-task-frontend-gilt.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+
+app.options("*", cors());
+
 app.use(express.json());
 
 // Routes
-const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/projects", require("./routes/projectRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
-const projectRoutes = require("./routes/projectRoutes");
-app.use("/api/projects", projectRoutes);
-
-const taskRoutes = require("./routes/taskRoutes");
-app.use("/api/tasks", taskRoutes);
-
-
-const userRoutes = require("./routes/userRoutes");
-app.use("/api/users", userRoutes);
-
-// Test route
+// Test
 app.get("/", (req, res) => {
-    res.send("API Running...");
+  res.send("API Running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });

35227b9602bdbcd4cb6170a946bc3306917004b6
Dileep Kumar <mdileepkumar578@gmail.com>
2026-05-04 20:02:12 +0530

    add project members feature

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,32 +1,20 @@
-const Project = require("../models/Project");
-
-// Create Project (Admin only)
 exports.createProject = async (req, res) => {
   try {
-    const { name, description } = req.body;
+    const { name, description, members } = req.body;
 
     const project = await Project.create({
       name,
       description,
       createdBy: req.user.id,
-      members: [req.user.id] // creator is member
+
+      
+      members: [
+        req.user.id,          
+        ...(members || [])    
+      ]
     });
 
     res.status(201).json(project);
-
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-// Get only user's projects
-exports.getProjects = async (req, res) => {
-  try {
-    const projects = await Project.find({
-      members: req.user.id   
-    }).populate("members", "name email");
-
-    res.json(projects);
 
   } catch (err) {
     res.status(500).json({ error: err.message });

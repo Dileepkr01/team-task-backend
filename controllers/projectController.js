ed1d474acaf31053c9fd99a97014d8460cf9a5b6
Dileep Kumar <mdileepkumar578@gmail.com>
2026-05-04 20:12:37 +0530

    fix members crash

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -2,21 +2,28 @@ exports.createProject = async (req, res) => {
   try {
     const { name, description, members } = req.body;
 
+    
+    let memberList = [];
+
+    if (Array.isArray(members)) {
+      memberList = members;
+    }
+
     const project = await Project.create({
       name,
       description,
       createdBy: req.user.id,
 
-      
       members: [
-        req.user.id,          
-        ...(members || [])    
+        req.user.id,
+        ...memberList
       ]
     });
 
     res.status(201).json(project);
 
   } catch (err) {
+    console.error("PROJECT ERROR:", err.message);
     res.status(500).json({ error: err.message });
   }
 };
